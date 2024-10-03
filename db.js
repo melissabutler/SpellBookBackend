b775ff514ae353323998dc9a4dde93cc779d893c
melissabutler <melissaebutler2015@gmail.com>
2024-10-03 11:28:32 -0400

    adjusting for production

--- db.js
+++ db.js
@@ -1,42 +1,42 @@
-// "use strict";
+"use strict";
 
-// //** Database setup for spellBook */
-// const { Client } = require("pg");
-// const { getDatabaseUri } = require("./config")
+//** Database setup for spellBook */
+const { Client } = require("pg");
+const { getDatabaseUri } = require("./config")
 
-// let db;
+let db;
 
-// if (process.env.NODE_ENV === "production") {
-//     db = new Client({
-//         connectionString: getDatabaseUri(),
-//         ssl: {
-//             rejectUnauthorized: false
-//         }
-//     });
-// } else {
-//     db = new Client({
-//         connectionString: getDatabaseUri()
-//     });
-// }
-
-// db.connect();
-
-// module.exports = db;
-
-const  { Client } = require("pg");
-
-let DB_URI;
-
-if(process.env.NODE_ENV === "test") {
-    DB_URI = "spellBook_test";
+if (process.env.NODE_ENV === "production") {
+    db = new Client({
+        connectionString: getDatabaseUri(),
+        ssl: {
+            rejectUnauthorized: false
+        }
+    });
 } else {
-    DB_URI = "spellBook"
+    db = new Client({
+        connectionString: getDatabaseUri()
+    });
 }
-
-const db = new Client({
-    host: "/var/run/postgresql",
-    database: DB_URI })
 
 db.connect();
 
 module.exports = db;
+
+// const  { Client } = require("pg");
+
+// let DB_URI;
+
+// if(process.env.NODE_ENV === "test") {
+//     DB_URI = "spellBook_test";
+// } else {
+//     DB_URI = "spellBook"
+// }
+
+// const db = new Client({
+//     host: "/var/run/postgresql",
+//     database: DB_URI })
+
+// db.connect();
+
+// module.exports = db;
