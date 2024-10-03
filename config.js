8c9d9bf34f5cbfb16e5f0f5698d6a0c8c1400370
melissabutler <melissaebutler2015@gmail.com>
2024-10-03 12:56:21 -0400

    troubleshooting

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const PORT = +process.env.PORT || 3001;
 function getDatabaseUri() {
     return (process.env.NODE_ENV === "test")
     ? "postgresql:///spellBook_test"
-    : process.env.DATABASE_URL || "postgresql://melissabutler:catbutts@127.0.0.1:5432/spellBook"
+    : process.env.DATABASE_URL || "postgresql:///spellBook"
 }
 
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
